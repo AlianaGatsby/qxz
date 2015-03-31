647e14517ecb726e5b83a0fba62944e0d2eb43b5
yanbangliu <1163579133@qq.com>
2015-03-31 16:43:44 +0800

    Right 1

--- app/controllers/qxz_controller.rb
+++ app/controllers/qxz_controller.rb
@@ -1,4 +1,20 @@
 class QxzController < ApplicationController
   def index
+  	@zhu={
+  		"4"=>"child_cold.png",
+  		"5"=>"child_asthma.png",
+  		"8"=>"adult_cold.png",
+  		"10"=>"old_cold.png",
+  		"12"=>"lung_disease.png",
+  		"15"=>"null.png",
+  	}
+  	@zi={
+  		"4"=>"儿童感冒气象风险",
+  		"5"=>"儿童哮喘气象风险",
+  		"8"=>"成人感冒气象风险",
+  		"10"=>"老人感冒气象风险",
+  		"12"=>"慢性阻塞性肺炎气象风险",
+  		"15"=>"",
+  	}
   end
 end
